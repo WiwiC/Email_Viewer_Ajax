611224c897ee5a1f06fd36bb2d1b7a8fbabd2c61
William <william.chalons@gmail.com>
2017-08-31 14:39:07 +0200

    Scaffold + strating CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |n|
+  objet  = Faker::Lorem.sentence(1)
+  paraph1 = Faker::Lorem.paragraph(2)
+  paraph2 = Faker::Lorem.paragraph(2)
+  paraph3 = Faker::Lorem.paragraph(2)
+  e = Email.create!(object:  objet,
+              body: '<p>'+paraph1+'</p><p>'+paraph2+'</p><p>'+paraph3+'</p>')
+end
